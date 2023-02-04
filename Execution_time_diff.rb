e3a927e4d221ee5e667235f53bc97a0f00d1de6f
useNavigate <106133580+useNavigate@users.noreply.github.com>
2023-02-04 01:58:57 -0500

    largest_contiguous_subsum

--- Execution_time_diff.rb
+++ Execution_time_diff.rb
@@ -1,4 +1,5 @@
-require 'byebug'
+require "byebug"
+
 # def my_min(arr)
 #     swapped = true
 #     arr1 = arr.dup
@@ -14,17 +15,60 @@ require 'byebug'
 #     arr1[0]
 # end
 
+#O(n^2)
+
 def my_min2(arr)
-    min = arr.first
-    arr.each do |el|
-        if el < min
-            min = el
-        end
+  min = arr.first
+  arr.each do |el|
+    if el < min
+      min = el
     end
-    min
+  end
+  min
 end
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min2(list)  # =>  -5
+list = [0, 3, 5, 4, -5, 10, 1, 90]
+p "my_min2(list):phase2 => #{my_min2(list)}"  # =>  -5
+#O(n)
 
+def largest_contiguous_subsum_1(list)
+  #get all the sub_arrs
+  array = []
+  (0...list.length).each do |i|
+    (i...list.length).each do |j|
+      array << list[i..j]
+    end
+  end
 
+  sum = 0
+  array.each do |sub|
+    if sub.reduce(:+) > sum
+      sum = sub.reduce(:+)
+    end
+  end
+  sum
+end
+
+# list = [5, 3, -7]
+list = [2, -2, 7, 5]
+
+p "largest_contiguous_subsum(list) : phase_1 => #{largest_contiguous_subsum_1(list)}"
+
+def largest_contiguous_subsum(arr)
+  max_sum = 0
+  current_sum = 0
+
+  arr.each do |num|
+    current_sum += num
+    if current_sum > max_sum
+      max_sum = current_sum
+    end
+    if current_sum < 0
+      current_sum = 0
+    end
+  end
+
+  max_sum
+end
+
+p largest_contiguous_subsum_2(list)
